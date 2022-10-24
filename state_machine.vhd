6a8fe887c37522a27c6cb58fcebe2447519d9985
eo20203 <78903047+eo20203@users.noreply.github.com>
2022-10-24 13:26:36 +0100

    Update state_machine.vhd

--- state_machine.vhd
+++ state_machine.vhd
@@ -9,7 +9,27 @@ library gaisler;
 use gaisler.misc.all;
 library UNISIM;
 use UNISIM.VComponents.all;
-entity state_machine is
+entity AHB_bridge is
+  port(
+    -- Clock and Reset -----------------
+    clkm : in std_logic;
+    rstn : in std_logic;
+    -- AHB Master records --------------
+    ahbmi : in ahb_mst_in_type;
+    ahbmo : out ahb_mst_out_type;
+     -- ARM Cortex-M0 AHB-Lite signals -- 
+    HADDR : in std_logic_vector (31 downto 0); -- AHB transaction address
+    HSIZE : in std_logic_vector (2 downto 0); -- AHB size: byte, half-word or word
+    HTRANS : in std_logic_vector (1 downto 0); -- AHB transfer: non-sequential only
+    HWDATA : in std_logic_vector (31 downto 0); -- AHB write-data
+    HWRITE : in std_logic; -- AHB write control
+    HRDATA : out std_logic_vector (31 downto 0); -- AHB read-data
+    HREADY : out std_logic -- AHB stall signal
+  );
+end; 
+architecture structural of AHB_bridge is
+--declare a component for state_machine
+component state_machine is
   port (
     clkm : in std_logic; -- Clock.
     rstn : in std_logic; -- Synchronous reset.
@@ -22,45 +42,63 @@ entity state_machine is
     HWDATA : in std_logic_vector (31 downto 0);
     HREADY : out std_logic
     );
-end;
-architecture dataflow of state_machine is
-  type state_type is (idle, instr_fetch);
-  signal curState, nextState:state_type;
-  begin
-    state_trans: process(curState, nextState)
-    begin
-      CASE curState is
-      when idle =>
-        if rstn = '1' then
-          nextState <= idle;
-        elsif HTRANS = "10" then
-          nextState <= instr_fetch;
-        else
-          nextState <= idle;
-        end if;
-      when instr_fetch =>
-        if rstn = '1' then
-          nextState <= idle;
-        elsif dmao.ready = '1' then
-          nextState <= idle;
-        else
-          nextState <= instr_fetch;
-        end if;
-      when OTHERS =>
-        nextState <= idle;
-      end case;
-    end process;
-    state_out: process(curState)
-    begin
-      CASE curState is
-      when idle =>
-        HREADY <= '1';
-        dmai.start <= '0';
-      when instr_fetch =>
-        HREADY <= '0';
-        dmai.start <= '1';
-      when OTHERS =>
-      end case;
-    end process;
-end;
+end component;
+
+
+--declare a component for ahbmst 
+component ahbmst is
+generic (
+  hindex  : integer := 0;
+  hirq    : integer := 0;
+  venid   : integer := VENDOR_GAISLER;
+  devid   : integer := 0;
+  version : integer := 0;
+  chprot  : integer := 3;
+  incaddr : integer := 0); 
+  port (
+    rst  : in  std_ulogic;
+    clk  : in  std_ulogic;
+    dmai : in ahb_dma_in_type;
+    dmao : out ahb_dma_out_type;
+    ahbi : in  ahb_mst_in_type;
+    ahbo : out ahb_mst_out_type 
+    );
+end component;
+
+--declare a component for data_swapper 
+
+component data_swapper is
+  port (
+    reset : in std_logic;
+    dmao_data : in std_logic_vector (31 downto 0);
+    hrdata : out std_logic_vector (31 downto 0)
+    );
+end component;
+
+signal dmai : ahb_dma_in_type;
+signal dmao : ahb_dma_out_type;
+begin
+--instantiate state_machine component and make the connections
+inst_state_machine : state_machine
+  port map(
+    clkm => clkm,
+    rstn => rstn,
+    dmao => dmao,
+    dmai => dmai,
+    HSIZE => HSIZE,
+    HTRANS => HTRANS,
+    HADDR => HADDR,
+    HWRITE => HWRITE,
+    HWDATA => HWDATA,
+    HREADY => HREADY
+  );
+--instantiate the ahbmst component and make the connections 
+--instantiate the data_swapper component and make the connections
+inst_data_swapper : data_swapper
+  port map(
+    reset => rstn,
+    dmao_data => dmao.rdata,
+    hrdata => HRDATA
+  );
+end structural;
 
